a072cb2fa1a6c97fad31cbd6a0d95f180aaa029e
COMPUS025\daw1 <agutierrezp18@educantabria.es>
2025-10-01 09:52:24 +0200

    actividad 1 hasta ejercicio 10

--- UF1_ACTVIDAD_01/src/Main.java
+++ UF1_ACTVIDAD_01/src/Main.java
@@ -8,12 +8,14 @@ public class Main {
         System.out.println("Ejercicio 01");
         System.out.println("Buenos días futuros programadores");
 
+
         // Ejercicio 2
         System.out.println("Ejercicio 02");
         double lado = 5;
         double area = lado * lado;
         System.out.println("lado: " + lado);
         System.out.println("area: " + area);
+
 
         // Ejercicio 3
         System.out.println("Ejercicio 03");
@@ -25,8 +27,10 @@ public class Main {
         System.out.println("lado: " + lado2);
         System.out.println("area: " + area2);
 
+
         //Ejercicio 4
         System.out.println("Ejercicio 04");
+        sc=new Scanner(System.in);
         System.out.println("Introduce el primer numero: ");
         double numero1 = sc.nextDouble();
         System.out.println("Introduce el segundo numero: ");
@@ -40,10 +44,98 @@ public class Main {
         System.out.println("La multiplicacion es: " + multiplicacion);
         System.out.println("La division es: " + division);
 
+
         //Ejercicio 05
         System.out.println("Ejercicio 05");
+        sc=new Scanner(System.in);
         System.out.println("Introduce longitud de radio: ");
         double radio = sc.nextDouble();
+        double perimetro= 2 * Math.PI * radio;
+        double areaC = Math.PI * radio * radio;
+        double volumeE = (4/3) * Math.pow(radio,3)* Math.PI;
+        System.out.println("El perimetro es: " + perimetro);
+        System.out.println("El area del radio es: " + areaC);
+        System.out.println("El volumen es: " + volumeE);
+
+
+        //Ejercicio 06
+        System.out.println("Ejercicio 06");
+        sc=new Scanner(System.in);
+        System.out.println("Introduce el precio del articulo");
+        double precio = sc.nextDouble();
+        System.out.println("Introduce el precio de venta real");
+        double precio2 = sc.nextDouble();
+        double descuento = precio - precio2;
+        double porcentaje = descuento / precio;
+        double porcentaje2 = porcentaje * 100;
+        System.out.println("El porcentaje de descuento es: " + porcentaje2 + "%");
+
+
+        //Ejercicio 07
+        System.out.println("Ejercicio 07");
+        sc=new Scanner(System.in);
+        System.out.println("Introduce distancia en millas marinas");
+        double distancia = sc.nextDouble();
+        double distancia2 = distancia * 1852;
+        System.out.println("La distancia en metros es: " + distancia2);
+
+
+        //Ejercicio 08
+        System.out.println("Ejercicio 08");
+        sc=new Scanner(System.in);
+        System.out.println("Introduce primer numero");
+        double num1 = sc.nextDouble();
+        System.out.println("Introduce segundo numero");
+        double num2 = sc.nextDouble();
+        System.out.println("Orden ascendente:" + Math.min(num1,num2) + "," + Math.max(num1,num2));
+
+
+        //Ejercicio 09
+        System.out.println("Ejercicio 09");
+        sc=new Scanner(System.in);
+        System.out.println("Introduce primer numero");
+        double Num1 = sc.nextDouble();
+        System.out.println("Introduce segundo numero");
+        double Num2 = sc.nextDouble();
+        System.out.println("Cual es mayor:" + Math.max(Num1,Num2));
+        System.out.println("El numero es igual:" + (Num1 == Num2));
+
+
+        //Ejercicio 10
+        System.out.println("Ejercicio 10");
+        sc=new Scanner(System.in);
+        System.out.println("Introduce primer numero");
+        double Núm1 = sc.nextDouble();
+        System.out.println("Introduce segundo numero");
+        double Núm2 = sc.nextDouble();
+        System.out.println("Introduce tercer numero");
+        double Núm3 = sc.nextDouble();
+        double
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
